6d48eac144b1f73bffe7a7e67121a6c671af5173
soul <celestiallie@gmail.com>
2023-10-04 17:16:38 +0800

    node

--- src/Components/Record/index.jsx
+++ src/Components/Record/index.jsx
@@ -210,48 +210,46 @@ function Record() {
             <div className="record">
                 <div className="wrapper">
                     {/* <div className="overlap"></div> */}
-                    <dialog className="dialog dialog__sale" ref={refDialogsale}>
-                        <div className="wrapper">
-                            <div className="dialog__menu">
-                                <h5 className="dialog__title">新增銷售</h5>
-                                <span className="material-symbols-outlined dialog__close">
-                                    Close
-                                </span>
-                            </div>
-                            <SaleForm salesState={salesState} />
-                        </div>
-                    </dialog>
-                    <div className="record__operate">
-                        <div className="record__operate__insert">
-                            {/* <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="btn btn-sm btn-secondary selling"> */}
-                            <button type="button" className="btn btn-sm btn-secondary dialog__sale__open">
-                                新增銷售
-                            </button>
-                        </div>
-                        <input className="record__operate__input" type="text" ref={refSearch} onChange={handleSearch} />
-                        <div className="record__operate__groupview" style={toggleGroupViewShow}>
-                            <div className="groupview__wrapper">
-                                <div className="groupview__menu">
-                                    <svg className="groupview__close" style={toggleSearchClose} onClick={searchDelete} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
-                                        <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
-                                    </svg>
-                                    {/* <span className="material-symbols-outlined groupview__close" style={toggleSearchClose} onClick={searchDelete}>Close</span> */}
+                    <div className="record__sidebar">
+                        <dialog className="dialog dialog__sale" ref={refDialogsale}>
+                            <div className="wrapper">
+                                <div className="dialog__menu">
+                                    <h5 className="dialog__title">新增銷售</h5>
+                                    <span className="material-symbols-outlined dialog__close">
+                                        Close
+                                    </span>
                                 </div>
-                                <GroupView filteredSale={filteredSale} groupViewProps={groupViewProps} />
+                                <SaleForm salesState={salesState} />
+                            </div>
+                        </dialog>
+                        <div className="record__operate">
+                            <div className="record__operate__insert">
+                                {/* <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="btn btn-sm btn-secondary selling"> */}
+                                <button type="button" className="btn btn-sm btn-secondary dialog__sale__open">
+                                    新增銷售
+                                </button>
+                            </div>
+                            <div className="record__operate__input">
+                                <input id="" type="text" ref={refSearch} onChange={handleSearch} />
+                                <svg class="feather feather-search" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="11" cy="11" r="8" /><line x1="21" x2="16.65" y1="21" y2="16.65" /></svg>
+                            </div>
+                            <div className="record__operate__groupview" style={toggleGroupViewShow}>
+                                <div className="groupview__wrapper">
+                                    <div className="groupview__menu">
+                                        <svg className="groupview__close" style={toggleSearchClose} onClick={searchDelete} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                                            <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
+                                        </svg>
+                                        {/* <span className="material-symbols-outlined groupview__close" style={toggleSearchClose} onClick={searchDelete}>Close</span> */}
+                                    </div>
+                                    <GroupView filteredSale={filteredSale} groupViewProps={groupViewProps} />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    {/* <div className="record__insert">
-                        <div className="record__insert__menu">                        
-                            <button type="button" className="btn btn-sm btn-secondary dialog__sale__open">
-                                新增銷售
-                            </button>
-                        </div>
-                    </div> */}
-                    <div className="record__overview">
                         <input id="datepicker" />
+                    </div>
+                    <section className="record__view">
                         <div className="record__overview__view">
-                            {isEmpty(salesState.dbSale) ? <div>No Data</div> :
+                            {!isEmpty(salesState.dbSale) &&
                                 <>
                                     {salesState.dbSale.map(sale => {
                                         return <Sale key={sale.id} sale={sale} salesState={salesState} remove={remove} />
@@ -260,7 +258,7 @@ function Record() {
                                 </>
                             }
                         </div>
-                    </div>
+                    </section>
                 </div>
             </div>
             {/* <Popup salesState={salesState} /> */}
